b5399e9ebd0a6f6a7d2b71300196e94c9b3020c0
Lillegul <camillal1@live.no>
2026-09-22 12:07:41 +0200

    add go to profile page btn

--- src/pages/profilePage/profilePage.ts
+++ src/pages/profilePage/profilePage.ts
@@ -1,30 +1,35 @@
 import { createPost } from "../../components/createPost";
-import type { Post } from "../../hooks/getAllPosts";
+import type { Post, PostsResponse } from "../../hooks/getAllPosts";
+import type { PostRequest } from "../../hooks/postPost";
+import type { FollowProfileResponse, Profile } from "../../hooks/profiles/follow-unfollow-profile";
 import { getProfilePosts } from "../../hooks/profiles/getProfilePosts";
+import { localStorageUtil } from "../../utils/storageUtils";
 // import "../../style/cards.css";
 
+//TODO BK HELP - red lines, 
+async function renderProfilepage() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let profileName = urlParams.get("name");
 
-
-
-async function renderProfilepage(){
-
-const urlParams = new URLSearchParams(window.location.search);
-const profileName = urlParams.get("name");
-
-
-if(profileName === null)
-  {return}
-
+  if (profileName === null) {
+    const user: Profile | null = localStorageUtil.load("user");
+    // console.log("user", user.data.name)
+    if (user) {
+      profileName = user.data.name;
+    }
+  }
 
   const profilePostResponse = await getProfilePosts(profileName);
-  // console.log(profilePost) 
-   const profile = profilePostResponse.data[0].author
-  console.log(profile) 
+  // console.log(profilePost)
+  const profile = profilePostResponse.data[0].author;
+  console.log(profile);
 
-const profileImgDiv = document.getElementById("profile-img") as HTMLDivElement
+  const profileImgDiv = document.getElementById(
+    "profile-img",
+  ) as HTMLDivElement;
 
-profileImgDiv.classList.add("profile-img-div")
- const userImage = document.createElement("img") as HTMLImageElement;
+  profileImgDiv.classList.add("profile-img-div");
+  const userImage = document.createElement("img") as HTMLImageElement;
   if (profile.avatar) {
     //TODO: default image wont work
     userImage.src = profile.avatar?.url;
@@ -33,25 +38,25 @@ profileImgDiv.classList.add("profile-img-div")
   }
   profileImgDiv.append(userImage);
 
-const profileNameElement = document.getElementById("profile-name") as HTMLHeadingElement
-profileNameElement.classList.add("profile-name")
-profileNameElement.textContent=profile.name
+  const profileNameElement = document.getElementById(
+    "profile-name",
+  ) as HTMLHeadingElement;
+  profileNameElement.classList.add("profile-name");
+  profileNameElement.textContent = profile.name;
 
-
-const profilePostsContainer = document.getElementById("profile-posts-container") as HTMLDivElement
-
+  const profilePostsContainer = document.getElementById(
+    "profile-posts-container",
+  ) as HTMLDivElement;
 
   // const postParent = document.getElementById("post-parent") as HTMLElement;
 
-
   function displayProfilePosts(posts: Post[], postsParent: HTMLElement) {
-      postsParent.innerHTML = "";
-      posts.forEach((post: Post) => {
-        createPost(post, postsParent );
-      });
+    postsParent.innerHTML = "";
+    posts.forEach((post: Post) => {
+      createPost(post, postsParent);
+    });
   }
-  displayProfilePosts(profilePostResponse.data, profilePostsContainer)
-
+  displayProfilePosts(profilePostResponse.data, profilePostsContainer);
 }
 
-renderProfilepage()
+renderProfilepage();
