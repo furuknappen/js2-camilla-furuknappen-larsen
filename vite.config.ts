2cf05c77c3858384a9a596326dddb6a78985e3ef
Lillegul <camillal1@live.no>
2026-09-25 15:18:15 +0200

    add config and fix pathing issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-  base: '/js2-camilla-furuknappen-larsen/',
-})
+  base: "/js2-camilla-furuknappen-larsen/",
+  build: {
+    rolldownOptions: {
+      input: {
+        main: path.resolve(import.meta.dirname, "index.html"),
+        createPost: path.resolve(
+          import.meta.dirname,
+          "src/pages/createPostPage/createPost.html",
+        ),
+        singlePost: path.resolve(
+          import.meta.dirname,
+          "src/pages/postPage/singlePostPage.html",
+        ),
+        profilePage: path.resolve(
+          import.meta.dirname,
+          "src/pages/profilePage/profilePage.html",
+        ),
+        login: path.resolve(import.meta.dirname, "src/pages/loginPage/login.html"),
+        register: path.resolve(
+          import.meta.dirname,
+          "src/pages/registerPage/registerPage.html",
+        ),
+      },
+    },
+  },
+});
